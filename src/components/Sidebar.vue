469da72b094a7ae8f1601f176cc03e82f91fce99
jose <joseojlo95@gmail.com>
2025-03-14 02:36:38 +0100

    Crea componente Lista De Tareas. Borra componentes autogenerados. Crea rutas

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="sidebar">
+  <h2>Barra lateral</h2>
+  <ul>
+    <li><RouterLink to="/contador">Contador</RouterLink></li>
+    <li><RouterLink to="/lista-de-tarea">Lista de tareas</RouterLink></li>
+  </ul>
+
+</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+.sidebar{
+  width: 200px;
+  background-color: #f0f0f0;
+  padding: 20px;
+}
+</style>
